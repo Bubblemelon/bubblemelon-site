cc8d9139f69ed1c40a5262c84b47c7ad4bc9b240
bubblemelon <c.f.2y.93@gmail.com>
2018-02-14 14:05:57 -0800

    added js to remove attributes from adobe spark rendering

---
+++ docs/assets/js/adobe-spark-css-edit.js
@@ -0,0 +1,12 @@
+// The following will remove all the attributes from the children of #aspark
+
+// Prevents JQuery code from running before the document is finished loading:
+$(document).ready( function() {
+
+var v = $("#aspark").find("div");
+v.removeAttr();
+
+});
+
+// could simply use
+// $(function() { ...code goes here... });
